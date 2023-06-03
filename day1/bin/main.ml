d277fd98063528bed6bbef3f153919bd5fa4cbd8
Stephen Barton <bartonstephen3@gmail.com>
2023-06-03 18:02:52 -0400

    Part 1

--- day1/bin/main.ml
+++ day1/bin/main.ml
@@ -1,17 +1,29 @@
+open Core
+
 let file = "day1.txt"
 
-let process line =
-  print_endline line;
+let safeConverter line =
+  match line with
+  | "" -> None
+  | _ -> Some (int_of_string line)
 ;;
 
-let read_lines file process =
-  let ic = open_in file in
-  let rec read_line () =
-    let line = try input_line ic with End_of_file -> exit 0
-    in
-      process line;
-      read_line ();
-  in read_line ()
+let read file = List.map (In_channel.read_lines file) ~f:safeConverter
+
+let rec sumElves (lst: int option list) res: int list =
+  match lst with
+  | [] -> res
+  | None :: tl -> sumElves tl (0 :: res)
+  | Some cal :: tl -> sumElves tl (
+    match res with
+    | [] -> [cal]
+    | x :: xs -> (x + cal) :: xs
+  )
 ;;
 
-let () = read_lines file process;;
+(* Find max in list *)
+let maxValue (lst : int list) : int = List.fold lst ~init:Int.min_value ~f:max
+;;
+
+let elvesCalories = sumElves (read file) [] in
+Out_channel.printf "Max calories: %d\n" (maxValue elvesCalories)
