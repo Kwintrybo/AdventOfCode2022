bc92020772c2481f4ae54f2c2e64103bd7d1656b
Stephen Barton <bartonstephen3@gmail.com>
2023-06-03 18:20:21 -0400

    Part 2, day 1 solution

--- day1/bin/main.ml
+++ day1/bin/main.ml
@@ -1,6 +1,7 @@
 open Core
 
 let file = "day1.txt"
+;;
 
 let safeConverter line =
   match line with
@@ -25,5 +26,23 @@ let rec sumElves (lst: int option list) res: int list =
 let maxValue (lst : int list) : int = List.fold lst ~init:Int.min_value ~f:max
 ;;
 
-let elvesCalories = sumElves (read file) [] in
+let elvesCalories = sumElves (read file) []
+;;
+
 Out_channel.printf "Max calories: %d\n" (maxValue elvesCalories)
+
+(* Copied code *)
+let rec max3 elvesCalories (m1, m2, m3) =
+  match elvesCalories with
+  | [] -> (m1, m2, m3)
+  | hd :: tl -> max3 tl (
+    match (m1, m2, m3) with
+    | m1, m2, _ when hd > m1 -> (hd, m1, m2)
+    | m1, m2, _ when hd > m2 -> (m1, hd, m2)
+    | m1, m2, m3 when hd > m3 -> (m1, m2, hd)
+    | _ -> (m1, m2, m3)
+  )
+;;
+
+let (m1, m2, m3) = max3 elvesCalories (Int.min_value, Int.min_value, Int.min_value) in
+Out_channel.printf "Max 3 values: %d, %d, %d\nTotal sum = %d\n" m1 m2 m3 (m1 + m2 + m3)
